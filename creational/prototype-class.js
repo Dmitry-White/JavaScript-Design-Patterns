574563c64768c328ccabb40fd5abae92a5fbe431
Dmitry Beloichuk <dmitry.b.white@gmail.com>
2019-11-14 15:50:39 +0300

    Add Prototype pattern, prototype version

--- creational/prototype-class.js
+++ creational/prototype-class.js
@@ -16,3 +16,20 @@ const civic = new Car(4, 'V6', 'grey');
 console.log('Class Entity: ', civic);
 console.log('Class Method: ', civic.drive());
 // -------------------------------------------------
+
+// ------- Prototype way, no Object.create ---------
+function Human(sex, name, age) {
+  this.sex = sex;
+  this.name = name;
+  this.age = age;
+}
+
+Human.prototype.say = function say() {
+  return `${this.name} says Hi.`;
+};
+
+const person = new Human('male', 'Mark', 32);
+
+console.log('Prototype Entity: ', person);
+console.log('Prototype Method: ', person.say());
+// -------------------------------------------------
