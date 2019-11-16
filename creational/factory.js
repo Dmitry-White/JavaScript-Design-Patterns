818a8c8acf5be88885cd21b792b5cbcf3d46a458
Dmitry Beloichuk <dmitry.b.white@gmail.com>
2019-11-16 15:41:09 +0300

    Add factory implementation

---
+++ creational/factory.js
@@ -0,0 +1,32 @@
+class CoolCar {
+  constructor(doors, engine, color) {
+    this.doors = doors;
+    this.engine = engine;
+    this.color = color;
+  }
+
+  drive() {
+    return `${this.engine} noises...`;
+  }
+}
+
+class CarFactory {
+  createCar(type) {
+    switch(type) {
+      case 'mazda':
+        return new CoolCar(4, 'V5', 'red');
+      case 'bmw':
+        return new CoolCar(4, 'V10', 'black');
+    }
+  }
+}
+
+const factory = new CarFactory();
+const mazda = factory.createCar('mazda');
+const bmw = factory.createCar('bmw');
+
+console.log('Factory-made Class Entity: ', mazda);
+console.log('Factory-made Class Method: ', mazda.drive());
+
+console.log('Factory-made Class Entity: ', bmw);
+console.log('Factory-made Class Method: ', bmw.drive());
