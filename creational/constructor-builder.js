f54186c7cf4941904d898a1ada135efeae3816a3
Dmitry Beloichuk <dmitry.b.white@gmail.com>
2019-11-16 14:55:01 +0300

    Add constructor implementation

---
+++ creational/constructor-builder.js
@@ -0,0 +1,28 @@
+class BuilderCar {
+  constructor(doors, engine, color) {
+    this.doors = doors;
+    this.engine = engine;
+    this.color = color;
+  }
+
+  drive() {
+    return `${this.engine} noises...`;
+  }
+}
+
+class SUV extends BuilderCar {
+  constructor(doors, engine, color) {
+    super(doors, engine, color);
+    this.wheels = 4;
+  }
+
+}
+
+const buiderCivic = new BuilderCar(4, 'V6', 'grey');
+const cx5 = new SUV(4, 'V6', 'grey');
+
+console.log('Class Entity: ', buiderCivic);
+console.log('Class Method: ', buiderCivic.drive());
+
+console.log('SubClass Entity: ', cx5);
+console.log('SubClass Method: ', cx5.drive());
