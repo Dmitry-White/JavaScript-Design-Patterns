1b156fd8de5bddb9478246091bbcbf0f4e28954c
Dmitry Beloichuk <dmitry.b.white@gmail.com>
2019-12-04 01:53:45 +0300

    Add initial mixin pattern

---
+++ structural/mixin.js
@@ -0,0 +1,23 @@
+class Car {
+  constructor(doors, engine, color) {
+    this.doors = doors;
+    this.engine = engine;
+    this.color = color;
+  }
+
+  drive() {
+    return `${this.engine} noises...`;
+  }
+}
+
+const myMixin = {
+  accelerate() {
+    return `${this.engine} gives hell to Greta!`
+  }
+}
+
+const civic = new Car(4, 'V6', 'grey');
+Object.assign(Car.prototype, myMixin);
+
+console.log('Class Entity: ', civic);
+console.log('Class Mixin: ', civic.accelerate());
