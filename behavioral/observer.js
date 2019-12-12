730c8a7cd202a4c2cf70437d3425c764025b2e05
Dmitry Beloichuk <dmitry.b.white@gmail.com>
2019-12-12 14:16:34 +0300

    Add observer pattern

---
+++ behavioral/observer.js
@@ -0,0 +1,43 @@
+// ----------------------- Subject / Publisher ------------------------
+const News = {
+  observers: [],
+  subscribe(subscriber) {
+    this.observers.push(subscriber);
+  },
+  unsubscribe(subscriber) {
+    const index = this.observers.indexOf(subscriber);
+    this.observers.splice(index, index);
+  },
+  notify(data) {
+    this.observers.forEach(subscriber => subscriber.receive(data));
+  },
+}
+// -------------------------------------------------------------------
+
+// --------------------- Observer / Subscriber -----------------------
+const NewsOutlet = {
+  init(name) {
+    this.name = name;
+    this.news = '';
+  },
+  receive(data) {
+      this.news = data;
+      this.reportNews();
+  },
+  reportNews() {
+    console.log(`${this.name}: ${this.news}`);
+  }
+}
+// -------------------------------------------------------------------
+
+const news = Object.create(News);
+const NYTimes = Object.create(NewsOutlet);
+const Guardian = Object.create(NewsOutlet);
+
+NYTimes.init('New York Times');
+Guardian.init('Guardian');
+
+news.subscribe(NYTimes);
+news.subscribe(Guardian);
+
+news.notify('Aussies are on fire!');
