295b9b7986e53d68b265debb5541ac2a75d294f7
Dmitry Beloichuk <dmitry.b.white@gmail.com>
2019-12-13 18:50:46 +0300

    Add object version of chain of responsibility pattern

--- behavioral/chain.js
+++ behavioral/chain.js
@@ -7,16 +7,17 @@ class ClassAccount {
     if (paymentPossible) {
       console.log(`Class: Paid ${amount} using ${this.name}`);
     } else if (handlerExists) {
-      console.log(`Class: Insufficient funds in ${this.name} account.`)
+      console.log(`Class: Insufficient funds in ${this.name} account.`);
       this.handler.pay(amount);
     } else {
       console.log('Class: Insufficient funds in all accounts.');
     }
-
   }
+
   isPaymentPossible(amount) {
     return this.balance >= amount;
   }
+
   next(handler) {
     this.handler = handler;
   }
@@ -25,7 +26,7 @@ class ClassAccount {
 class ClassVisa extends ClassAccount {
   constructor(balance) {
     super();
-    this.name = 'Visa'
+    this.name = 'Visa';
     this.balance = balance;
   }
 }
@@ -54,4 +55,59 @@ classVisa.next(classPaypal);
 classPaypal.next(classQiwi);
 
 classVisa.pay(200);
+// ---------------------------------------------------------------
+
+
+// --------------------- Object version --------------------------
+const accountObj = {
+  pay(amount) {
+    const paymentPossible = this.isPaymentPossible(amount);
+    const handlerExists = this.handler;
+
+    if (paymentPossible) {
+      console.log(`Object: Paid ${amount} using ${this.name}`);
+    } else if (handlerExists) {
+      console.log(`Object: Insufficient funds in ${this.name} account.`);
+      this.handler.pay(amount);
+    } else {
+      console.log('Object: Insufficient funds in all accounts.');
+    }
+  },
+  isPaymentPossible(amount) {
+    return this.balance >= amount;
+  },
+  next(handler) {
+    this.handler = handler;
+  },
+};
+
+function initialize(name, balance) {
+  this.name = name;
+  this.balance = balance;
+}
+
+const visaObj = Object.create(accountObj);
+visaObj.init = function init(balance) {
+  return initialize.apply(this, ['Visa', balance]);
+};
+
+const paypalObj = Object.create(accountObj);
+paypalObj.init = function init(balance) {
+  return initialize.apply(this, ['Paypal', balance]);
+};
+
+const qiwiObj = Object.create(accountObj);
+qiwiObj.init = function init(balance) {
+  return initialize.apply(this, ['Qiwi', balance]);
+};
+
+visaObj.init(100);
+paypalObj.init(200);
+qiwiObj.init(500);
+
+visaObj.next(paypalObj);
+paypalObj.next(qiwiObj);
+
+visaObj.pay(400);
+
 // ---------------------------------------------------------------
