0c492561fb64b05c2ec15db8cc0972a996c182fd
Dmitry Beloichuk <dmitry.b.white@gmail.com>
2019-12-14 19:43:40 +0300

    Add object version of iterator pattern

---
+++ behavioral/iterator.js
@@ -0,0 +1,21 @@
+const iterable = {
+  [Symbol.iterator]() {
+    let step = 0;
+    const length = 3;
+    const iterator = {
+      next() {
+        step += 1;
+        return step < length 
+          ? { value: step, done: false }
+          : { value: null, done: true }
+      }
+    }
+    return iterator;
+  }
+}
+
+const iterableObj = iterable[Symbol.iterator]();
+
+console.log('Iterable: ', iterableObj);
+console.log('Next iteration: ', iterableObj.next());
+console.log('Next iteration: ', iterableObj.next());
